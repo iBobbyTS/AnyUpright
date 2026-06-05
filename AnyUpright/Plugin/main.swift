55e10f8ab26637ba72bbb526317f92113c0fadec
iBobbyTS <ibobbyts@gmail.com>
2026-06-05 00:52:06 -0600

    Initial Commit

---
+++ AnyUpright/Plugin/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  AnyUpright
+//
+//  Created by iBobby on 2026-06-05.
+//
+
+FxPrincipal.startServicePrincipal()
+
---
+++ AnyUpright/Wrapper Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  AnyUpright
+//
+//  Created by iBobby on 2026-06-05.
+//
+
+import AppKit
+
+@NSApplicationMain
+@objc(AppDelegate) class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window : NSWindow!;
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        // Insert code here to initialize your application
+        
+    }
+}
